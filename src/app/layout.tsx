0249b78bf5e81490f53185d4dda465b3b674df35
rapha <onanaraphaelnathan@gmail.com>
2026-05-10 21:44:34 -0400

    fix overflow-x on browsers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -130,7 +130,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             data-scroll-behavior="smooth"
             className={`${sans.variable} ${mono.variable} h-full antialiased`}
         >
-            <body className="min-h-full flex flex-col">
+            <body className="min-h-full flex flex-col overflow-x-hidden">
                 <Providers>{children}</Providers>
                 <Analytics />
                 <SpeedInsights />
